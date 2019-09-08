d8348d06dd68c514652468546fbe8020b4836174
Krzysztof Baraniecki <krzysztof.webyou@gmail.com>
2019-09-08 12:15:35 +0200

    meble na wymiar

--- index.php
+++ index.php
@@ -65,14 +65,100 @@
 
     </section>
 
-        <section id="na-wymiar" class="text-white">
+        <section id="na-wymiar" class="text-white furnituresSize">
             <div class="container">
                 <h2 class="h1">Meble na wymiar</h2>
                 <div class="row">
-                    <div class="col-12 col-md">1</div>
-                    <div class="col-12 col-md">2</div>
-                    <div class="col-12 col-md">3</div>
-                    <div class="col-12 col-md">4</div>
+                    <div class="col-12 col-md-5 offset-md-1 furnituresSize__content">
+                        <div class="furnituresSize__box">
+                            <div class="furnituresSize__image">
+                                <img src="images/sections/meble-na-wymiar/kuchenne.jpg" alt="" class="img-fluid">
+                            </div>
+                            <div class="furnituresSize__title">
+                                <h3>Kuchenne</h3>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-12 col-md-5 offset-md-6 furnituresSize__content">
+                        <div class="furnituresSize__box">
+                            <div class="furnituresSize__image">
+                                <img src="images/sections/meble-na-wymiar/szafy-i-wneki.jpg" alt="" class="img-fluid">
+                            </div>
+                            <div class="furnituresSize__title">
+                                <h3>Szafy i wnęki</h3>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-12 col-md-5 offset-md-1 furnituresSize__content">
+                        <div class="furnituresSize__box">
+                            <div class="furnituresSize__image">
+                                <img src="images/sections/meble-na-wymiar/lazienki.jpg" alt="" class="img-fluid">
+                            </div>
+                            <div class="furnituresSize__title">
+                                <h3>Łazienki</h3>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-12 col-md-5 offset-md-6 furnituresSize__content">
+                        <div class="furnituresSize__box">
+                            <div class="furnituresSize__image">
+                                <img src="images/sections/meble-na-wymiar/pokoj.jpg" alt="" class="img-fluid">
+                            </div>
+                            <div class="furnituresSize__title">
+                                <h3>Pokój</h3>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-12 col-md-5 offset-md-1 furnituresSize__content">
+                        <div class="furnituresSize__box">
+                            <div class="furnituresSize__image">
+                                <img src="images/sections/meble-na-wymiar/przedpokoj.jpg" alt="" class="img-fluid">
+                            </div>
+                            <div class="furnituresSize__title">
+                                <h3>Przedpokój</h3>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-12 col-md-5 offset-md-6 furnituresSize__content">
+                        <div class="furnituresSize__box">
+                            <div class="furnituresSize__image">
+                                <img src="images/sections/meble-na-wymiar/mlodziezowe-i-dzieciece.jpg" alt="" class="img-fluid">
+                            </div>
+                            <div class="furnituresSize__title">
+                                <h3>Młodzieżowe i dziecięce</h3>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-12 col-md-5 offset-md-1 furnituresSize__content">
+                        <div class="furnituresSize__box">
+                            <div class="furnituresSize__image">
+                                <img src="images/sections/meble-na-wymiar/sypialnia.jpg" alt="" class="img-fluid">
+                            </div>
+                            <div class="furnituresSize__title">
+                                <h3>Sypialnia</h3>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-12 col-md-5 offset-md-6 furnituresSize__content">
+                        <div class="furnituresSize__box">
+                            <div class="furnituresSize__image">
+                                <img src="images/sections/meble-na-wymiar/dla-firm.jpg" alt="" class="img-fluid">
+                            </div>
+                            <div class="furnituresSize__title">
+                                <h3>Dla firm</h3>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-12 col-md-5 offset-md-1 furnituresSize__content">
+                        <div class="furnituresSize__box">
+                            <div class="furnituresSize__image">
+                                <img src="images/sections/meble-na-wymiar/pozostale.jpg" alt="" class="img-fluid">
+                            </div>
+                            <div class="furnituresSize__title">
+                                <h3>Pozostałe</h3>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </section>
