0c942569e222ff7fd2ba8c5aa34482f6a73438a5
Mazur <andrzej.webyou@gmail.com>
2019-09-08 10:45:42 +0200

    JS poprawiony -
    trzeba pamietac aby aktywoac body !

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
     <?php echo $template_start_head; ?>
     <title>Meble Księżyk</title>
     <?php echo $template_end_head; ?>
-    <body data-spy="scroll" data-target="#navbar" data-offset="400">
+    <body data-spy="scroll" data-target="#navbar" data-offset="400" class="body" id="body">
         <header id="header">
             <?php echo $nav_menu; ?>
             <!-- <div id="slider" class="text-center">
@@ -25,52 +25,50 @@
             </div>
         </div>
     </section>
-    <section id="secondary">
-        <section id="secondary__onas">
-            <div class="secondary__title">
+    <section id="onas">
+        <div class="slant-top"></div>
+        
+            <div class="container">
+            <div class="onas__title">
                 <h2 class="h2">O nas</h2>
             </div>
-            <div class="container">
-                    <div class="row secondary__row">
-                        <div class="col-sm-12 col-lg-4 d-flex secondary__column-1">
+                    <div class="row onas__row">
+                        <div class="col-sm-12 col-lg-4 d-flex onas__column-1">
                             <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.<br><br>
                                 Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu.<br><br>
                                 In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat.</p>
                         </div>
-                        <div class="col-sm-12 col-lg-4 d-block secondary__column-2">
-                            <div class="secondary__column-2-inside">
+                        <div class="col-sm-12 col-lg-4 d-block onas__column-2">
+                            <div class="onas__column-2-inside">
                             <h5 class="first">Wyjątkowy dizajn</h5>
                             <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Lorem ipsum dolor sit amet.</p>
                             </div> 
-                            <div class="secondary__column-2-inside">
+                            <div class="onas__column-2-inside">
                             <h5 class="second">Doświadczenie</h5>
                             <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Lorem ipsum dolor sit amet.</p>
 
                             </div>
-                            <div class="secondary__column-2-inside">
+                            <div class="onas__column-2-inside">
                             <h5 class="third">Gwarancja jakości</h5>
                             <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Lorem ipsum dolor sit amet.</p>
                             </div>
                         </div>
-                        <div class="col-sm-12 col-lg-4 d-flex secondary__column-3">
-                            <img src="../mebleksiezyk/images/sections/onas/rzybornik.jpg">
+                        <div class="col-sm-12 col-lg-4 d-flex onas__column-3">
+                            <img src="../mebleksiezyk/images/sections/onas/1.jpg">
                         </div>
                     </div>
             </div>
-        </section>
+       
 
 
 
 
-        <!-- <section>
-        x
-        </section> -->
     </section>
 
         <section id="na-wymiar" class="text-white">
             <div class="container">
                 <h2 class="h1">Meble na wymiar</h2>
-                <div class="rwo">
+                <div class="row">
                     <div class="col-12 col-md">1</div>
                     <div class="col-12 col-md">2</div>
                     <div class="col-12 col-md">3</div>
@@ -83,7 +81,7 @@
         <section id="uslugi">
             <div class="container">
                 <h2 class="h1">Naszym klientom zapewniamy</h2>
-                <div class="rwo">
+                <div class="row">
                     <div class="col-12 col-md">1</div>
                     <div class="col-12 col-md">2</div>
                     <div class="col-12 col-md">3</div>
