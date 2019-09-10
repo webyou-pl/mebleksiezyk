3b01a55c4b18593686f05f0b977557d5095708f0
Krzysztof Baraniecki <krzysztof.webyou@gmail.com>
2019-09-10 20:38:43 +0200

    add faficon

--- structure/template.php
+++ structure/template.php
@@ -20,6 +20,15 @@ $template_end_head = '
     <link rel="mask-icon" href="'.$address.'/images/safari-pinned-tab.svg" color="#e1a1e1">
     <meta name="msapplication-TileColor" content="#e1a1e1">
     <meta name="theme-color" content="#ffffff">
+
+    <link rel="apple-touch-icon" sizes="180x180" href="'.$address.'/images/favicon//apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="'.$address.'/images/favicon//favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="'.$address.'/images/favicon//favicon-16x16.png">
+    <link rel="manifest" href="'.$address.'/images/favicon//site.webmanifest">
+    <link rel="mask-icon" href="'.$address.'/images/favicon//safari-pinned-tab.svg" color="#5bbad5">
+    <meta name="msapplication-TileColor" content="#000000">
+    <meta name="theme-color" content="#000000">
+
 </head>
 ';
 
